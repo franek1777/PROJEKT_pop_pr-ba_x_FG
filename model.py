a9d4ff46004f4e9ece29a6f11dc74c5afa830b3d
franek1777 <u64730@student.wat.edu.pl>
2025-06-13 08:33:57 +0200

    wip:update

---
+++ model.py
@@ -0,0 +1,18 @@
+sports_facilities: list[dict] = [
+    {'sports_facilities_type': 'boisko', 'sports_facilities_location': 'Olsztyn'},
+    {'sports_facilities_type': 'stadion', 'sports_facilities_location': 'Kraków'},
+    {'sports_facilities_type': 'basen', 'sports_facilities_location': 'Wrocław'},
+    {'sports_facilities_type': 'orlik', 'sports_facilities_location': 'Słupsk'}
+]
+
+clients: list[dict] = [
+    {'client_name': 'Kamil Pora', 'client_sports': 'tenis', 'client_location1': 'Kraków', 'client_location2': 'Hrubieszów'},
+    {'client_name': 'Patryk Aura', 'client_sports': 'piłka_nożna', 'client_location1': 'Słupsk', 'client_location2': 'Godno'}
+]
+
+workers: list[dict] = [
+    {'worker_name': 'Monika', 'worker_sports_facilities': 'boisko', 'worker_location': 'Słupsk'},
+    {'worker_name': 'Andrij', 'worker_sports_facilities': 'stadion', 'worker_location': 'Lublin'},
+    {'worker_name': 'Wiesław', 'worker_sports_facilities': 'basen', 'worker_location': 'Kraków'},
+    {'worker_name': 'Jarosław', 'worker_sports_facilities': 'orlik', 'worker_location': 'Hrubieszów'}
+]
